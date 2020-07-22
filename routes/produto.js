58a7f45064d3d6d2a7c445be32f9ff22540e51c6
DiegoMagalhaesProgrammer <digengup@gmail.com>
2020-07-21 22:45:23 -0300

    66

--- routes/produto.js
+++ routes/produto.js
@@ -7,7 +7,7 @@ express.get('/', (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error }) }
         conn.query(
-            'SELECT *FROM PRODUTO WHERE NOME = "?";',
+            'SELECT * FROM produto;',
            // [req.body.nome],
             (error, resultado, fields) => {
                 if (error) { return res.status(500).send({ error: error }) }
